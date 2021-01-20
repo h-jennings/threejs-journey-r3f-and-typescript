2c569fb51d63a646513dd509d1253a6b5e154f1f
h-jennings <jenningsdhunter@gmail.com>
2021-01-20 13:00:32 -0500

    adding start files and deps

---
+++ 08-fullscreen-and-resizing/src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Canvas } from 'react-three-fiber';
+import { Mesh } from 'three';
+import { OrbitControls } from 'drei';
+
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+interface CubeProps {
+  color: number | string;
+}
+const Cube: React.FC<CubeProps> = ({ color }) => {
+  const mesh = React.useRef<Mesh>();
+  return (
+    <mesh position={[0, 0, 0]} ref={mesh}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshBasicMaterial color={color} />
+    </mesh>
+  );
+};
+
+const App: React.FC = () => {
+  return (
+    <Canvas
+      style={{ width: sizes.width, height: sizes.height }}
+      gl={{ alpha: false }}
+      camera={{
+        position: [0, 0, 3],
+        fov: 75,
+        aspect: sizes.width / sizes.height,
+      }}>
+      <OrbitControls />
+      <Cube color={0xff0000} />
+    </Canvas>
+  );
+};
+
+export default App;

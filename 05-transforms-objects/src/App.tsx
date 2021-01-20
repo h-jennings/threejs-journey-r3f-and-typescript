c95afd05731a88453c3ca3aa6427420b40c209ec
Hunter Jennings <jenningsdhunter@gmail.com>
2021-01-19 23:06:50 -0500

    translating code to r3f

--- 05-transforms-objects/src/App.tsx
+++ 05-transforms-objects/src/App.tsx
@@ -7,17 +7,36 @@ const sizes = {
   height: 600,
 };
 
-const Box: React.FC = (props) => {
+interface CubeProps {
+  color: number | string;
+  posX: number;
+}
+const Cube: React.FC<CubeProps> = (props) => {
   const mesh = React.useRef<Mesh>();
   return (
-    <mesh {...props} ref={mesh}>
+    <mesh {...props} position={[props.posX, 0, 0]} ref={mesh}>
       <boxGeometry args={[1, 1, 1]} />
-      <meshBasicMaterial color={0xff0000} />
+      <meshBasicMaterial color={props.color} />
     </mesh>
   );
 };
 
-function App() {
+const Cubes: React.FC = (props) => {
+  return (
+    <group
+      {...props}
+      position={[0, 1, 0]}
+      scale={[1, 2, 1]}
+      rotation={[0, 1, 0]}
+    >
+      <Cube posX={0} color={0xff0000} />
+      <Cube posX={-2} color={0x00ff00} />
+      <Cube posX={2} color={0x0000ff} />
+    </group>
+  );
+};
+
+const App: React.FC = () => {
   return (
     <Canvas
       style={{ width: sizes.width, height: sizes.height }}
@@ -25,11 +44,12 @@ function App() {
       camera={{
         position: [0, 0, 3],
         fov: 75,
+        aspect: sizes.width / sizes.height,
       }}
     >
-      <Box />
+      <Cubes />
     </Canvas>
   );
-}
+};
 
 export default App;

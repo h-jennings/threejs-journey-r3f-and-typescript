adf0c6e9d9884eb123093ac8f57c7e55316cfe3e
Hunter Jennings <jenningsdhunter@gmail.com>
2021-01-24 20:31:53 -0500

    implementing lesson code in r3f

--- 14-lights/src/App.tsx
+++ 14-lights/src/App.tsx
@@ -1,83 +1,113 @@
 import React, { Suspense } from 'react';
-import { Canvas, useResource, useUpdate, useLoader } from 'react-three-fiber';
 import {
-  FontLoader,
-  MeshMatcapMaterial,
-  MeshStandardMaterial,
-  TextBufferGeometry,
-  TextureLoader,
-  TorusBufferGeometry,
-} from 'three';
+  Canvas,
+  useResource,
+  useUpdate,
+  useThree,
+  MeshProps,
+  useFrame,
+} from 'react-three-fiber';
+import * as THREE from 'three';
 import { OrbitControls } from '@react-three/drei';
 
-interface TextProps {
-  material: any;
+interface SharedMeshProps {
+  material: THREE.Material;
 }
-const Text: React.FC<TextProps> = ({ children, material }) => {
-  const font = useLoader(FontLoader, '/fonts/helvetiker_regular.typeface.json');
-  const config = React.useMemo(
-    () => ({
-      font,
-      size: 0.5,
-      height: 0.2,
-      curveSegments: 4,
-      bevelEnabled: true,
-      bevelThickness: 0.03,
-      bevelSize: 0.02,
-      bevelOffset: 0,
-      bevelSegments: 5,
-    }),
-    [font],
-  );
-  const geometry = useUpdate<TextBufferGeometry>(
-    (self) => {
-      self?.center();
-    },
-    [children],
-  );
-  return (
-    <mesh material={material}>
-      <textBufferGeometry ref={geometry} args={[String(children), config]} />
-    </mesh>
-  );
-};
-interface DonutsProps {
-  material: MeshStandardMaterial;
-}
-const torus = new TorusBufferGeometry(0.3, 0.2, 20, 45);
-const Donuts: React.FC<DonutsProps> = ({ material }) => {
-  return (
-    <>
-      {Array(500)
-        .fill(null)
-        .map((_, idx) => {
-          const scale = Math.random();
-          return (
-            <mesh
-              position={[
-                (Math.random() - 0.5) * 10,
-                (Math.random() - 0.5) * 10,
-                (Math.random() - 0.5) * 10,
-              ]}
-              rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
-              scale={[scale, scale, scale]}
-              key={idx}
-              geometry={torus}
-              material={material}
-            />
-          );
-        })}
-    </>
-  );
+const MeshWithRotation: React.FC<MeshProps & SharedMeshProps> = (props) => {
+  const { material } = props;
+  const ref = React.useRef<THREE.Mesh>();
+  useFrame((_, delta) => {
+    if (ref.current) {
+      ref.current.rotation.y += 0.1 * delta;
+      ref.current.rotation.x += 0.15 * delta;
+    }
+  });
+  return <mesh {...props} ref={ref} material={material} />;
 };
 
 const Objects: React.FC = () => {
-  const material = useResource<MeshStandardMaterial>();
-  const matcap = useLoader(TextureLoader, '/textures/matcaps/8.png');
+  const material = useResource<THREE.MeshStandardMaterial>();
+
   return (
     <mesh>
-      <meshMatcapMaterial ref={material} matcap={matcap} />
+      <meshStandardMaterial ref={material} roughness={0.4} />
+      {material.current ? (
+        <group>
+          <MeshWithRotation material={material.current} position={[-1.5, 0, 0]}>
+            <sphereBufferGeometry args={[0.5, 32, 32]} />
+          </MeshWithRotation>
+          <MeshWithRotation material={material.current}>
+            <boxBufferGeometry args={[0.75, 0.75, 0.75]} />
+          </MeshWithRotation>
+          <MeshWithRotation material={material.current} position={[1.5, 0, 0]}>
+            <torusBufferGeometry args={[0.3, 0.2, 32, 64]} />
+          </MeshWithRotation>
+          <mesh
+            material={material.current}
+            position={[0, -0.65, 0]}
+            rotation={[-Math.PI * 0.5, 0, 0]}>
+            <planeBufferGeometry args={[5, 5]} />
+          </mesh>
+        </group>
+      ) : null}
     </mesh>
+  );
+};
+
+const Lights: React.FC = () => {
+  const { scene } = useThree();
+
+  const directionalLightRef = useResource<THREE.DirectionalLight>();
+
+  const ral = useUpdate<THREE.RectAreaLight>((self) => {
+    self.lookAt(new THREE.Vector3());
+  }, []);
+  const sl = useUpdate<THREE.SpotLight>((self) => {
+    self.target.position.x = -0.75;
+    scene.add(self.target);
+  }, []);
+
+  React.useEffect(() => {
+    let request: number;
+
+    if (sl.current) {
+      // * Creating helper
+      const spotLightHelper = new THREE.SpotLightHelper(sl.current);
+      scene.add(spotLightHelper);
+
+      // * SpolightHelper is weird and needs to be updated next frame
+      // * in order to have the correct position.
+      request = requestAnimationFrame(() => spotLightHelper.update());
+    }
+
+    // * Clean up RAF
+    return () => cancelAnimationFrame(request);
+  }, [sl, scene]);
+
+  return (
+    <>
+      <ambientLight args={[0xffffff, 0.5]} />
+      <directionalLight
+        ref={directionalLightRef}
+        args={[0x00ffcc, 0.3]}
+        position={[1, 0.25, 0]}
+      />
+      {directionalLightRef.current && (
+        <directionalLightHelper args={[directionalLightRef.current, 0.2]} />
+      )}
+      <hemisphereLight args={[0xff0000, 0x0000ff, 0.3]} />
+      <pointLight args={[0xff9000, 0.5, 10, 2]} position={[1, -0.5, 1]} />
+      <rectAreaLight
+        ref={ral}
+        args={[0x4e00ff, 2, 1, 1]}
+        position={[-1.5, 0, 1.5]}
+      />
+      <spotLight
+        ref={sl}
+        args={[0x78ff00, 0.5, 6, Math.PI * 0.1, 0.25, 1]}
+        position={[0, 2, 3]}
+      />
+    </>
   );
 };
 
@@ -94,7 +124,7 @@ const App: React.FC = () => {
       pixelRatio={Math.min(window.devicePixelRatio, 2)}>
       <color attach='background' args={[0, 0, 0]} />
       <OrbitControls />
-      <ambientLight />
+      <Lights />
       <Suspense fallback={null}>
         <Objects />
       </Suspense>
